63490cc3d8482306f3f8c87a912eaea8535d11ec
Md.Sadiq <mohammadsadiq4950@gmail.com>
2026-04-20 21:00:18 +0530

    cron final touch

--- pkg/cron.go
+++ pkg/cron.go
@@ -3,8 +3,9 @@ package pkg
 import (
 	"context"
 	"fmt"
+	"syscall/js"
+	"time"
 
-	"github.com/MdSadiqMd/issue-tracker/internal"
 	"github.com/syumai/workers/cloudflare"
 	"github.com/syumai/workers/cloudflare/cron"
 )
@@ -17,11 +18,61 @@ func CronTask(ctx context.Context) error {
 	fmt.Printf("Cron task started at %v\n", e.ScheduledTime)
 
 	cloudflare.WaitUntil(func() {
-		_, err := internal.FetchIssuesLogic()
-		if err != nil {
+		url := "http://localhost:8787/send-report"
+
+		headers := js.Global().Get("Object").New()
+		headers.Set("Content-Type", "application/json")
+
+		options := js.Global().Get("Object").New()
+		options.Set("method", "POST")
+		options.Set("headers", headers)
+
+		fetchFunc := js.Global().Get("fetch")
+		promise := fetchFunc.Invoke(url, options)
+
+		resultChan := make(chan []byte, 1)
+		errorChan := make(chan error, 1)
+
+		thenFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			response := args[0]
+			status := response.Get("status").Int()
+
+			if status != 200 {
+				textPromise := response.Call("text")
+				textPromise.Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+					errorText := args[0].String()
+					errorChan <- fmt.Errorf("endpoint returned status %d: %s", status, errorText)
+					return nil
+				}))
+				return nil
+			}
+
+			jsonPromise := response.Call("json")
+			jsonPromise.Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				jsonStr := js.Global().Get("JSON").Call("stringify", args[0]).String()
+				resultChan <- []byte(jsonStr)
+				return nil
+			}))
+
+			return nil
+		})
+
+		catchFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			errorChan <- fmt.Errorf("fetch failed: %v", args[0].String())
+			return nil
+		})
+		promise.Call("then", thenFunc)
+		promise.Call("catch", catchFunc)
+
+		select {
+		case data := <-resultChan:
+			fmt.Printf("Cron task completed successfully: %s\n", string(data))
+
+		case err := <-errorChan:
 			fmt.Printf("Cron task error: %v\n", err)
-		} else {
-			fmt.Println("Cron task completed successfully")
+
+		case <-time.After(30 * time.Second):
+			fmt.Printf("Cron task timeout\n")
 		}
 	})
 	return nil
